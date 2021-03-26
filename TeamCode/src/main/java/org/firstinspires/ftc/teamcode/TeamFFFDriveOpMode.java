853747585c5cccf5165be36f0fde37f1bf7411dd
wfrfred <865188661@qq.com>
2021-03-26 16:34:28 +0800

    增加了可变速微调模式 修复了各类bug

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamFFFDriveOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamFFFDriveOpMode.java
@@ -8,6 +8,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @TeleOp(name="TeamFFFDriveOpMode", group="FFF")
 public class TeamFFFDriveOpMode extends OpMode {
     private DcMotor l1,l2,r1,r2;
+    private double k;
 
     @Override
     public void init() {
@@ -15,6 +16,7 @@ public class TeamFFFDriveOpMode extends OpMode {
         l2 = hardwareMap.dcMotor.get("l2");
         r1 = hardwareMap.dcMotor.get("r1");
         r2 = hardwareMap.dcMotor.get("r2");
+        k = 1;
 
         //麦轮左右反转
         l1.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -24,15 +26,22 @@ public class TeamFFFDriveOpMode extends OpMode {
     @Override
     public void loop() {
         //读取手柄：前进 转向 横向
-        double move = -gamepad1.left_stick_y;
-        double fun = gamepad1.left_stick_x;
-        double turn= gamepad1.right_stick_x;
+        double move = gamepad1.left_stick_y;
+        double fun = -gamepad1.left_stick_x;
+        double turn= -gamepad1.right_stick_x;
+
+        if (gamepad1.right_bumper){
+            k = 0.25;
+        } else{
+            k = 1;
+        }
+
 
         //计算每个电机
-        double first = (move + turn + fun)/4;
-        double second = (move + turn - fun)/4;
-        double third = (move - turn - fun)/4;
-        double fourth = (move - turn + fun)/4;
+        double first = (move + turn + fun)*k;
+        double second = (move + turn - fun)*k;
+        double third = (move - turn - fun)*k;
+        double fourth = (move - turn + fun)*k;
 
         l1.setPower(first);
         l2.setPower(second);
