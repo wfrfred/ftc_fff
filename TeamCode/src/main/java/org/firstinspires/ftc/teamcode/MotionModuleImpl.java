70fe978af2bc9e74f575192bb691aaf3b5cd9966
wfrfred <865188661@qq.com>
2021-04-05 00:54:06 +0800

    修复了部分代码错误，以及注释格式错误

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotionModuleImpl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotionModuleImpl.java
@@ -1,4 +1,12 @@
 package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+    /**
+     * @Author RL
+     * @Time 2021/4/4  20:52
+     */
+
 public class MotionModuleImpl implements MotionModule{
     private DcMotorEx leftFront;
     private DcMotorEx leftBack;
@@ -9,10 +17,6 @@ public class MotionModuleImpl implements MotionModule{
     private double lBack = 0;
     private double rBack = 0;
     private boolean runningMode = false;
-    /*
-    @Author RL
-    @Time 2021/4/4  20:52
-    */
     public void changeSpeed(double anglespeed) {
         /*
         将前后平动的速度均匀分配到每个马达
@@ -41,18 +45,18 @@ public class MotionModuleImpl implements MotionModule{
         this.rFront= (move - turn - fun)*k;
         this.rBack = (move - turn + fun)*k;
 
-        double max=0;                           //找四个值中绝对值最大的值
-        if (math.abs(this.lFront)>math.abs(this.lBack)){
-            max = math.abs(this.lFront);
+        double max=0;
+        //找四个值中绝对值最大的值
+        if (Math.abs(this.lFront)>Math.abs(this.lBack)){
+            max = Math.abs(this.lFront);
+        } else{
+            max = Math.abs(this.lBack);
         }
-        else{
-            max = math.abs(this.lBack);
+        if (Math.abs(this.rFront)>Math.abs(max)){
+            max = Math.abs(this.rFront);
         }
-        if (math.abs(this.rFront)>math.abs(max)){
-            max = math.abs(this.rFront);
-        }
-        if (math.abs(this.rBack)>math.abs(max)){
-            max = math.abs(this.rBack);
+        if (Math.abs(this.rBack)>Math.abs(max)){
+            max = Math.abs(this.rBack);
         }
         
         if (max>1){
@@ -60,39 +64,44 @@ public class MotionModuleImpl implements MotionModule{
             this.lBack  = this.lBack  / max;
             this.rFront = this.rFront / max;
             this.rBack  = this.rBack  / max;
-        }                   //确保没有值越界（界为[-1,1]）
+        }
+        //确保没有值越界（界为[-1,1]）
         leftFront.setPower(this.lFront);
         rightFront.setPower(this.rFront);
         leftBack.setPower(this.lBack);
-        rightBack.setPower(this.rBack);//给每个马达设置速度
+        rightBack.setPower(this.rBack);
+        //给每个马达设置速度
     }
-    public double getState(String motor) {
-        if (runningMode){               //若手动，返回Power
-            if(motor == "leftFront"){
+    public double getState(String motor)throws IllegalArgumentException {
+        if (runningMode) {
+            //若手动，返回Power
+            if (motor == "leftFront") {
                 return lFront;
             }
-            if(motor == "leftBack"){
+            if (motor == "leftBack") {
                 return lBack;
             }
-            if(motor == "rightFront"){
+            if (motor == "rightFront") {
                 return rFront;
             }
-            if(motor == "rightBack"){
+            if (motor == "rightBack") {
                 return rBack;
             }
-        else{                           //若程序控制，返回角速度
-            if(motor == "leftFront"){
-                return leftFront.getVelocity()
+        }else {
+            //若程序控制，返回角速度
+            if (motor == "leftFront") {
+                return leftFront.getVelocity();
             }
-            if(motor == "leftBack"){
-                return leftBack.getVelocity()
+            if (motor == "leftBack") {
+                return leftBack.getVelocity();
             }
-            if(motor == "rightFront"){
-                return rightFront.getVelocity()
+            if (motor == "rightFront") {
+                return rightFront.getVelocity();
             }
-            if(motor == "rightBack"){
-                return rightBack.getVelocity()
+            if (motor == "rightBack") {
+                return rightBack.getVelocity();
             }
         }
+        throw(new IllegalArgumentException());
     }
 }
