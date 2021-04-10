4ee9460b4bf3ee85c4fe0d123269c384f07c9844
wfrfred <865188661@qq.com>
2021-04-10 20:47:15 +0800

    帮hyc写了下射击模块

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShootingModule.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShootingModule.java
@@ -1,4 +1,42 @@
 package org.firstinspires.ftc.teamcode;
 
+/**
+ * @author houyicheng2005
+ * @Time 2021-04-05 22:10
+ * @version 1.0
+ */
 public interface ShootingModule {
+    /**
+     * 用于发射
+     * 每次发射清空弹仓
+     * 次数为设定的子弹数
+     */
+    void shoot();
+
+    /**
+     * 用于瞄准
+     */
+    void aim();
+
+    /**
+     * 用于启动飞轮
+     */
+    void startMotor();
+
+    /**
+     * 用于停止飞轮
+     */
+    void stopMotor();
+
+    /**
+     * 获取子弹数
+     * @return 子弹数
+     */
+    int getBulletAmount();
+
+    /**
+     * 改变子弹数
+     * @param bulletAmount 要设定的子弹数 范围为0到3
+     */
+    void setBulletAmount(int bulletAmount);
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShootingModuleImpl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShootingModuleImpl.java
@@ -1,4 +1,68 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
 public class ShootingModuleImpl implements ShootingModule{
+    private DcMotor shootMotor;
+    private Servo pushServo;
+    private long startTime = 0;
+    private final float SHOOT_POSITION = 0.9f;
+    private int bulletAmount = 0;
+
+    ShootingModuleImpl(HardwareMap hardwareMap){
+        shootMotor = hardwareMap.dcMotor.get("shootMotor");
+        pushServo = hardwareMap.servo.get("pushServo");
+    }
+
+    public void shoot(){
+        if(startTime !=0&&System.currentTimeMillis()-startTime>1000){
+            while(bulletAmount !=0){
+                push(SHOOT_POSITION);
+                resetServo();
+            }
+        }else{
+
+        }
+    }
+
+    public void startMotor(){
+        shootMotor.setPower(1f);
+        startTime = System.currentTimeMillis();
+    }
+
+    public void stopMotor() {
+        shootMotor.setPower(0f);
+        startTime = 0;
+    }
+
+    public void aim(){
+        //等CVModule
+    }
+
+    private void push(double position){
+        pushServo.setPosition(position);
+        --bulletAmount;
+        while(pushServo.getPosition()==position) {
+            return;
+        }
+    }
+
+    private void resetServo(){
+        pushServo.setPosition(0f);
+        while(pushServo.getPosition()==0f) {
+            return;
+        }
+    }
+
+    public int getBulletAmount() {
+        return bulletAmount;
+    }
+
+    public void setBulletAmount(int bulletAmount) {
+        if(bulletAmount>=0&&bulletAmount<=3) {
+            this.bulletAmount = bulletAmount;
+        }
+    }
 }
